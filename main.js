14bda4abd13eac5d8081023995e70d0c01e364fc
Bastianu <35289464+Bastianu@users.noreply.github.com>
2020-09-25 15:22:34 +0200

    add db index main style

---
+++ main.js
@@ -0,0 +1,17 @@
+const technosDiv = document.querySelector('#technos');
+
+function loadTechnologies(technos) {
+    fetch('http://localhost:3001/technos')
+        .then(response => {
+            response.json()
+                .then(technos => {
+                    const allTechnos = technos.map(t => `<div><b>${t.name}</b> ${t.description}  <a href="${t.url}">site de ${t.name}</a> </div>`)
+                            .join('');
+            
+                    technosDiv.innerHTML = allTechnos; 
+                });
+        })
+        .catch(console.error);
+}
+
+loadTechnologies(technos);
